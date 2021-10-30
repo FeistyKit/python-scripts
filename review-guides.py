987f0b0b39f51a32ac232ee9b51ff80e41155ba3
FeistyKit <eeveebeevee33@gmail.com>
2021-10-30 16:13:32 -0400

    stuff`

--- review-guides.py
+++ review-guides.py
@@ -8,6 +8,7 @@ parser.add_argument("--casesensitive", "-c", action="store_const", const=True, d
 parser.add_argument("--shuffle", "-s", action="store_const", const=True, default=False)
 parser.add_argument("--output", "-o", action="store_const", const=True, default=False)
 parser.add_argument("--outfile", nargs=1, type=str)
+parser.add_argument("--newparse", "-n", action="store_const", const=True, default=False, help="Whether or not to use the new type of parsing")
 parser.add_argument("file", nargs=1, type=str)
 args = parser.parse_args()
 
@@ -16,7 +17,7 @@ if args.outfile is None:
 else:
     args.outfile = args.outfile[0]
 
-def ask_question(in_str, failed, num, to_save=None):
+def old_ask_question(in_str, failed, num, to_save=None):
     copied_str = in_str
     in_str = in_str.strip(" ;")
     items = in_str.split(args.delimiter)
@@ -47,6 +48,56 @@ def ask_question(in_str, failed, num, to_save=None):
         os.system("cls")
 
 
+def new_ask_question(in_str, failed, num, to_save=None):
+    copied_str = in_str
+    in_str = in_str.replace("%l", "")
+    in_str = in_str.strip(" ;")
+    items = in_str.split(args.delimiter)
+    items = list(map(lambda x: x.strip(), items))
+    list_version = re.match(r"^.*;\%l", copied_str)
+    if len(items) <= 1 and not list_version:
+        print(f"ERROR: {args.file[0]}:{num + 1} Item `{in_str}` does not have a delimiter!", file=sys.stderr)
+        return
+
+    times = len(re.findall(args.toreplace + "+", items[0]))
+    if times != len(items) - 1 and not list_version:
+        print( f"ERROR: {args.file[0]}:{num + 1} Item `{in_str}` has too many splits: it has `{times}` splits and only `{len(items) - 1}` items!", file=sys.stderr)
+        return
+
+    answers= [answer.strip() for answer in input(items[0] + ": ").split(",")]
+
+    correct = []
+    if list_version:
+        better_items = list(map(lambda x: x.lower(), items[1:]))
+        correct = [better_items for _ in range(times)]
+    else:
+        for f in items[1:]:
+            correct.append(list(map(lambda x: x.strip().lower(), list(f.split("|")))))
+
+    if not optionscmp(correct, list(map(lambda x: x.lower(), answers))):
+        if to_save is not None:
+            to_save.append(copied_str)
+
+        if len(items[1:]) > 1:
+            failed.append(f"For the question `{items[0]}`, `{', '.join(answers)}` was wrong! The correct answers were `{', '.join(items[1:])}`.")
+        else:
+            failed.append(f"For the question `{items[0]}`, `{', '.join(answers)}` was wrong! The correct answer was `{items[1]}`.")
+
+    if platform.system() == "Linux":
+        os.system("clear")
+    elif platform.system() == "Windows":
+        os.system("cls")
+
+def optionscmp(correct: list[list[str]], provided: list[str]):
+    try:
+        for i in range(len(correct)):
+            if provided[i] not in correct[i]:
+                return False
+    except IndexError:
+        return False
+    return True
+
+
 def run():
     content = None
     try:
@@ -56,7 +107,7 @@ def run():
         print(f"Could not open file `{args.file[0]}`!", file=sys.stderr)
         sys.exit(1)
 
-    questions = list(content.splitlines())
+    questions = list(enumerate(content.splitlines()))
     if args.shuffle:
         random.shuffle(questions)
 
@@ -68,8 +119,11 @@ def run():
     elif platform.system() == "Windows":
         os.system("cls")
 
-    for i, q in enumerate(questions):
-        ask_question(q, failed, i)
+    for i, q in questions:
+        if args.newparse:
+            new_ask_question(q, failed, i)
+        else:
+            old_ask_question(q, failed, i)
     if failed:
         print('\n'.join(failed))
         print(f"You got `{len(failed)}` wrong!", file=sys.stderr)
